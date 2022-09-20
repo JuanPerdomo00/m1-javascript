3988bb8e340a649309ce61475e24e2ede55cdf0b
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-09-20 17:22:58 -0500

    casi terminada

---
+++ 07-Algoritmos-I/homework/homework.js
@@ -0,0 +1,70 @@
+"use strict";
+// No cambies los nombres de las funciones.
+
+function factorear(num) {
+  // Factorear el número recibido como parámetro y devolver en un array
+  // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
+  // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
+  // Tu código:
+  let newArr = [];
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      console.log(i);
+      newArr.push(i);
+      num /= i;
+    }
+  }
+  return newArr;
+}
+
+function bubbleSort(array) {
+  // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
+  // el array recibido como parámetro
+  // Devolver el array ordenado resultante
+  // Tu código:
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+      }
+    }
+  }
+  return array;
+}
+
+function insertionSort(array) {
+  // Implementar el método conocido como insertionSort para ordenar de menor a mayor
+  // el array recibido como parámetro utilizando arreglos
+  // Devolver el array ordenado resultante
+  // Tu código:
+  for (let i = 1; i < array.length; i++) {
+    let aux = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
+}
+
+function selectionSort(array) {
+  // Implementar el método conocido como selectionSort para ordenar de menor a mayor
+  // el array recibido como parámetro utilizando dos arreglos
+  // Devolver el array ordenado resultante
+  // Tu código:
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  factorear,
+  bubbleSort,
+  insertionSort,
+  selectionSort,
+};
